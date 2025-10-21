79b0c45131096b24c852c590f4f0fca4e5e697ad
Gongjing <Jane112726g@gmail.com>
2025-10-21 10:31:22 +0800

    feat: support --s to shout

--- echo.py
+++ echo.py
@@ -1,6 +1,9 @@
+import sys
+
 def echo():
+    shout = "--s" in sys.argv
     message = input("Enter something: ")
-    print(message)
+    print(message.upper() if shout else message)
 
 if __name__ == "__main__":
     echo()
